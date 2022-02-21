35f29d8582c49eb42195fda8eba57b86b44f6b50
hasnamohamed <hasnaamohammed538@gmail.com>
2022-02-22 01:54:22 +0200

    edit receptionist middlware

--- app/Http/Middleware/receptionist.php
+++ app/Http/Middleware/receptionist.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class receptionist
 {
@@ -16,8 +17,9 @@ class receptionist
      */
     public function handle(Request $request, Closure $next)
     {
-        if ($request->role == 'receptionist') {
+        if (Auth::check() && Auth::user()->role == 'receptionist') {
                    return $next($request);
         }
+        abort(403);
     }
 }
