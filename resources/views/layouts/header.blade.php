4505ffc360928b6431e15b783fa3244e1c89562b
hasnamohamed <hasnaamohammed538@gmail.com>
2022-02-21 18:18:57 +0200

    show users to rece

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,48 @@
+<header>
+    <div class="topbar">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-8 text-sm">
+                    <div class="site-info">
+                        <a href="https://wa.me/+201010495597"><span class="mai-call text-primary"></span> +20 101 049
+                            5597</a>
+                        <span class="divider">|</span>
+                        <a href="https://mail.google.com/mail/?view=cm&fs=1&to=ahmedhafezoffic@gmail.com"><span
+                                class="mai-mail text-primary"></span> ahmedhafezoffic@.com</a>
+                    </div>
+                </div>
+                <div class="col-sm-4 text-right text-sm">
+                    <div class="social-mini-button">
+                        <a href="https://www.facebook.com/profile.php?id=100005116839262"><span
+                                class="mai-logo-facebook-f"></span></a>
+                        <a href="https://www.linkedin.com/in/ahmedhafez247/"><span class="mai-logo-linkedin"></span></a>
+                        <a href="https://github.com/AhmedHafez7-Eng"><span class="mai-logo-github"></span></a>
+                    </div>
+                </div>
+            </div> <!-- .row -->
+        </div> <!-- .container -->
+    </div> <!-- .topbar -->
+
+    <nav class="navbar navbar-expand-lg navbar-light shadow-sm">
+        <div class="container">
+            <a class="navbar-brand" href="{{url('/')}}"><span class="text-primary">Life</span>-Care</a>
+
+            <form action="#">
+                <div class="input-group input-navbar">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text" id="icon-addon1"><span class="mai-search"></span></span>
+                    </div>
+                    <input type="text" class="form-control" placeholder="Enter keyword.." aria-label="Username"
+                        aria-describedby="icon-addon1">
+                </div>
+            </form>
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupport"
+                aria-controls="navbarSupport" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+           
+        </div> <!-- .container -->
+    </nav>
+</header>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//admin routes
+Route::get('/show_users', [AdminController::class, 'show_users']);
+Route::get('/add_doctor', [AdminController::class, 'addDoctor'])->middleware(['auth'])->middleware('admin');
+Route::post('/add_doctor', [AdminController::class, 'createDoctor'])->middleware(['auth'])->middleware('admin');
+Route::get('/updateDoctor/{id}', [AdminController::class, 'updateDoctor'])->middleware(['auth'])->middleware('admin');
+Route::put('/edit_doctor/{id}', [AdminController::class, 'edit_doctor'])->middleware(['auth'])->middleware('admin');
+Route::delete('/deleteDoctor/{id}', [AdminController::class, 'deleteDoctor'])->middleware(['auth'])->middleware('admin');
+
+Route::get('/show_appointments', [AdminController::class, 'show_appointments'])->middleware(['auth'])->middleware('admin');
+Route::get('/canceled/{id}', [AdminController::class, 'canceled'])->middleware(['auth'])->middleware('admin');
+Route::get('/approved/{id}', [AdminController::class, 'approved'])->middleware(['auth'])->middleware('admin');
+
+
+Route::get('/emailNotify/{id}', [AdminController::class, 'emailNotify'])->middleware(['auth'])->middleware('admin');
+Route::post('/sendEmail/{id}', [AdminController::class, 'sendEmail'])->middleware(['auth'])->middleware('admin');
+
